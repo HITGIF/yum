dd50430c5dc390fc344f36b37ef951ece9adfaec
Mark Wang <me@anhai.wang>
2024-09-14 14:21:09 +0800

    .

--- lib/types/video_id.ml
+++ lib/types/video_id.ml
@@ -16,7 +16,7 @@ module Option = struct
 end
 
 module type S = sig
-  type t
+  type t [@@deriving sexp_of]
 
   val supported_url_formats : string list
   val of_string : string -> t
@@ -43,7 +43,7 @@ let find_prefix_and_curl =
 ;;
 
 module Youtube : S = struct
-  type t = string
+  type t = string [@@deriving sexp_of]
 
   let prefix_normal = "https://www.youtube.com/watch?v="
   let prefix_short = "https://youtu.be/"
@@ -72,6 +72,7 @@ module Bilibili : S = struct
     { video : string
     ; part : int option
     }
+  [@@deriving sexp_of]
 
   let prefix_normal = "https://www.bilibili.com/video/"
   let prefix_short = "https://b23.tv/"
@@ -113,7 +114,10 @@ module Bilibili : S = struct
   let to_url t = [%string "%{prefix_normal}%{to_string t}"]
 end
 
-type t = Youtube of Youtube.t [@@deriving variants]
+type t =
+  | Youtube of Youtube.t
+  | Bilibili of Bilibili.t
+[@@deriving variants, sexp_of]
 
 let supported_url_formats_msg =
   [ "> Supported `<url>` formats:" ]
@@ -125,8 +129,9 @@ let supported_url_formats_msg =
 let of_youtube_string = Fn.compose youtube Youtube.of_string
 
 let of_url url =
+  let open Option.Let_syntax in
   Fn.compose Option.map ~f:youtube Youtube.of_url url
-  |> Option.or_else ~f:(fun () -> None)
+  =? (fun () -> Fn.compose Option.map ~f:bilibili Bilibili.of_url url)
   |> function
   | Some x -> Ok x
   | None ->
@@ -135,65 +140,120 @@ let of_url url =
 ;;
 
 let to_url = function
-  | Youtube youtube -> Youtube.to_url youtube
+  | Youtube id -> Youtube.to_url id
+  | Bilibili id -> Bilibili.to_url id
 ;;
 
 let%test_module "_" =
   (module struct
+    let youtube_urls =
+      [ "https://www.youtube.com/watch?v=U7L-3VXAkSA"
+      ; "https://www.youtube.com/watch?v=oXZcuHIR5ko&pp=ygUJbG92ZSAyMDAw"
+      ; "   https://www.youtube.com/watch?v=oXZcuHIR5ko&pp=ygUJbG92ZSAyMDAw  "
+      ; " https://youtu.be/H767hjCLk5A"
+      ; " https://youtu.be/EUsG3oY4Cmo?si=x6u51-6NfhX5-okB "
+      ; "https://www.youtube.com/watch?v=qzoq8cpp6qI&list=LL&index=1"
+      ; "sakdlj lakdj lk https://www.youtube.com/watch?v=qzoq8cpp6qI&list=LL&index=1"
+      ]
+    ;;
+
+    let bilibili_urls =
+      [ "https://www.bilibili.com/video/BV1ez421X7Gz/?spm_id_from=333.999.0.0&vd_source=9e41e603371977727f00ca56687eaa1e"
+      ; "https://www.bilibili.com/video/BV1ez421X7Gz/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6&t=153"
+      ; "【【异世界情绪】日文翻唱《死ぬのがいいわ/不如死去》】 \
+         https://www.bilibili.com/video/BV1ez421X7Gz/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6"
+      ; "【【异世界情绪】CH4NGE【Candy Live2】】 【精准空降到 00:03】 \
+         https://www.bilibili.com/video/BV1S14y1T7uj/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6&t=3"
+      ; "https://b23.tv/9XSgbpt"
+      ; "https://www.bilibili.com/video/BV1nC8CetEeS/?vd_source=9e41e603371977727f00ca56687eaa1e&p=3"
+      ; "https://www.bilibili.com/video/BV1nC8CetEeS?p=8&vd_source=9e41e603371977727f00ca56687eaa1e"
+      ; "https://www.bilibili.com/video/BV1nC8CetEeS?p=14&vd_source=9e41e603371977727f00ca56687eaa1e"
+      ; "https://www.bilibili.com/video/BV1nC8CetEeS/?p=14&vd_source=9e41e603371977727f00ca56687eaa1e"
+      ; "https://www.bilibili.com/video/BV1nC8CetEeS/?p=7"
+      ]
+    ;;
+
     let%expect_test "youtube url normalization" =
       let test url =
         Youtube.(url |> of_url |> Option.value_exn |> to_url |> print_endline)
       in
-      test "https://www.youtube.com/watch?v=U7L-3VXAkSA";
-      [%expect {| https://www.youtube.com/watch?v=U7L-3VXAkSA |}];
-      test "https://www.youtube.com/watch?v=oXZcuHIR5ko&pp=ygUJbG92ZSAyMDAw";
-      [%expect {| https://www.youtube.com/watch?v=oXZcuHIR5ko |}];
-      test "   https://www.youtube.com/watch?v=oXZcuHIR5ko&pp=ygUJbG92ZSAyMDAw  ";
-      [%expect {| https://www.youtube.com/watch?v=oXZcuHIR5ko |}];
-      test " https://youtu.be/H767hjCLk5A";
-      [%expect {| https://www.youtube.com/watch?v=H767hjCLk5A |}];
-      test " https://youtu.be/EUsG3oY4Cmo?si=x6u51-6NfhX5-okB ";
-      [%expect {| https://www.youtube.com/watch?v=EUsG3oY4Cmo |}];
-      test "https://www.youtube.com/watch?v=qzoq8cpp6qI&list=LL&index=1";
-      [%expect {| https://www.youtube.com/watch?v=qzoq8cpp6qI |}];
-      test "sakdlj lakdj lk https://www.youtube.com/watch?v=qzoq8cpp6qI&list=LL&index=1";
-      [%expect {| https://www.youtube.com/watch?v=qzoq8cpp6qI |}]
+      List.iter youtube_urls ~f:test;
+      [%expect
+        {|
+        https://www.youtube.com/watch?v=U7L-3VXAkSA
+        https://www.youtube.com/watch?v=oXZcuHIR5ko
+        https://www.youtube.com/watch?v=oXZcuHIR5ko
+        https://www.youtube.com/watch?v=H767hjCLk5A
+        https://www.youtube.com/watch?v=EUsG3oY4Cmo
+        https://www.youtube.com/watch?v=qzoq8cpp6qI
+        https://www.youtube.com/watch?v=qzoq8cpp6qI |}]
     ;;
 
     let%expect_test "bilibili url normalization" =
       let test url =
         Bilibili.(url |> of_url |> Option.value_exn |> to_url |> print_endline)
       in
-      test
-        "https://www.bilibili.com/video/BV1ez421X7Gz/?spm_id_from=333.999.0.0&vd_source=9e41e603371977727f00ca56687eaa1e";
-      [%expect {| https://www.bilibili.com/video/BV1ez421X7Gz |}];
-      test
-        "https://www.bilibili.com/video/BV1ez421X7Gz/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6&t=153";
-      [%expect {| https://www.bilibili.com/video/BV1ez421X7Gz |}];
-      test
-        "【【异世界情绪】日文翻唱《死ぬのがいいわ/不如死去》】 \
-         https://www.bilibili.com/video/BV1ez421X7Gz/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6";
-      [%expect {| https://www.bilibili.com/video/BV1ez421X7Gz |}];
-      test
-        "【【异世界情绪】CH4NGE【Candy Live2】】 【精准空降到 00:03】 \
-         https://www.bilibili.com/video/BV1S14y1T7uj/?share_source=copy_web&vd_source=3e826ee3881d478c81bbfea52fdc92d6&t=3";
-      [%expect {| https://www.bilibili.com/video/BV1S14y1T7uj |}];
-      test "https://b23.tv/9XSgbpt";
-      [%expect {| https://www.bilibili.com/video/BV1wGvUeXEkr |}];
-      test
-        "https://www.bilibili.com/video/BV1nC8CetEeS/?vd_source=9e41e603371977727f00ca56687eaa1e&p=3";
-      [%expect {| https://www.bilibili.com/video/BV1nC8CetEeS/?p=3 |}];
-      test
-        "https://www.bilibili.com/video/BV1nC8CetEeS?p=8&vd_source=9e41e603371977727f00ca56687eaa1e";
-      [%expect {| https://www.bilibili.com/video/BV1nC8CetEeS/?p=8 |}];
-      test
-        "https://www.bilibili.com/video/BV1nC8CetEeS?p=14&vd_source=9e41e603371977727f00ca56687eaa1e";
-      [%expect {| https://www.bilibili.com/video/BV1nC8CetEeS/?p=14 |}];
-      test
-        "https://www.bilibili.com/video/BV1nC8CetEeS/?p=14&vd_source=9e41e603371977727f00ca56687eaa1e";
-      [%expect {| https://www.bilibili.com/video/BV1nC8CetEeS/?p=14 |}];
-      test "https://www.bilibili.com/video/BV1nC8CetEeS/?p=7";
-      [%expect {| https://www.bilibili.com/video/BV1nC8CetEeS/?p=7 |}]
+      List.iter bilibili_urls ~f:test;
+      [%expect
+        {|
+        https://www.bilibili.com/video/BV1ez421X7Gz
+        https://www.bilibili.com/video/BV1ez421X7Gz
+        https://www.bilibili.com/video/BV1ez421X7Gz
+        https://www.bilibili.com/video/BV1S14y1T7uj
+        https://www.bilibili.com/video/BV1wGvUeXEkr
+        https://www.bilibili.com/video/BV1nC8CetEeS/?p=3
+        https://www.bilibili.com/video/BV1nC8CetEeS/?p=8
+        https://www.bilibili.com/video/BV1nC8CetEeS/?p=14
+        https://www.bilibili.com/video/BV1nC8CetEeS/?p=14
+        https://www.bilibili.com/video/BV1nC8CetEeS/?p=7 |}]
+    ;;
+
+    let%expect_test "of_url" =
+      let test url = of_url url |> Or_error.sexp_of_t sexp_of_t |> print_s in
+      youtube_urls @ bilibili_urls @ [ ""; "aaa"; "kaln  klsjlkaj " ] |> List.iter ~f:test;
+      [%expect
+        {|
+        (Ok (Youtube U7L-3VXAkSA))
+        (Ok (Youtube oXZcuHIR5ko))
+        (Ok (Youtube oXZcuHIR5ko))
+        (Ok (Youtube H767hjCLk5A))
+        (Ok (Youtube EUsG3oY4Cmo))
+        (Ok (Youtube qzoq8cpp6qI))
+        (Ok (Youtube qzoq8cpp6qI))
+        (Ok (Bilibili ((video BV1ez421X7Gz) (part ()))))
+        (Ok (Bilibili ((video BV1ez421X7Gz) (part ()))))
+        (Ok (Bilibili ((video BV1ez421X7Gz) (part ()))))
+        (Ok (Bilibili ((video BV1S14y1T7uj) (part ()))))
+        (Ok (Bilibili ((video BV1wGvUeXEkr) (part ()))))
+        (Ok (Bilibili ((video BV1nC8CetEeS) (part (3)))))
+        (Ok (Bilibili ((video BV1nC8CetEeS) (part (8)))))
+        (Ok (Bilibili ((video BV1nC8CetEeS) (part (14)))))
+        (Ok (Bilibili ((video BV1nC8CetEeS) (part (14)))))
+        (Ok (Bilibili ((video BV1nC8CetEeS) (part (7)))))
+        (Error
+          "URL format is not supported.\
+         \n\
+         \n> Supported `<url>` formats:\
+         \n> - `[...]https://www.youtube.com/watch?v=<id>[...]`\
+         \n> - `[...]https://youtu.be/<id>[...]`\
+         \n> - `[...]https://www.bilibili.com/video/<id>[...]`\
+         \n> - `[...]https://b23.tv/<id>[...]`")
+        (Error
+          "URL format is not supported.\
+         \n\
+         \n> Supported `<url>` formats:\
+         \n> - `[...]https://www.youtube.com/watch?v=<id>[...]`\
+         \n> - `[...]https://youtu.be/<id>[...]`\
+         \n> - `[...]https://www.bilibili.com/video/<id>[...]`\
+         \n> - `[...]https://b23.tv/<id>[...]`")
+        (Error
+          "URL format is not supported.\
+         \n\
+         \n> Supported `<url>` formats:\
+         \n> - `[...]https://www.youtube.com/watch?v=<id>[...]`\
+         \n> - `[...]https://youtu.be/<id>[...]`\
+         \n> - `[...]https://www.bilibili.com/video/<id>[...]`\
+         \n> - `[...]https://b23.tv/<id>[...]`") |}]
     ;;
   end)
 ;;
