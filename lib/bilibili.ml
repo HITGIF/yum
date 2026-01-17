23d73b7fe23b936d41e1eab59ef7193ff7ea2343
hitgif <me@anhai.wang>
2026-01-17 23:37:19 +0800

    .

--- lib/bilibili.ml
+++ lib/bilibili.ml
@@ -12,9 +12,9 @@ let headers =
 
 let validate_response ~(here : [%call_pos]) response =
   match Cohttp.Response.status response with
-  | `OK -> Deferred.Or_error.ok_unit
+  | `OK -> Ok ()
   | status_code ->
-    Deferred.Or_error.error_s
+    Or_error.error_s
       [%message
         "Unexpected HTTP response code"
           (status_code : Cohttp.Code.status_code)
@@ -40,9 +40,14 @@ let decompress string =
   return output
 ;;
 
-let get_html url =
-  let%bind response, body = Cohttp_async.Client.get ~headers url in
-  let%bind.Deferred.Or_error () = validate_response response in
+let get_html html_url =
+  let%bind response, body = Cohttp_async.Client.get ~headers html_url in
+  let%bind.Deferred.Or_error () =
+    validate_response response
+    |> Or_error.tag_s_lazy
+         ~tag:[%lazy_sexp { html_url : string = Uri.to_string html_url }]
+    |> return
+  in
   let%bind body = Cohttp_async.Body.to_string body in
   decompress body
 ;;
@@ -57,10 +62,13 @@ let audio_resource_regex =
 
 module Audio_resource = struct
   type audio =
-    { backup_url : string list
+    { base_url : string
+    ; backup_url : string list
     ; bandwidth : int
     }
   [@@yojson.allow_extra_fields] [@@deriving sexp_of, of_yojson, fields ~getters]
+
+  let urls { base_url; backup_url; _ } = base_url :: backup_url
 
   type dash = { audio : audio list }
   [@@yojson.allow_extra_fields] [@@deriving sexp_of, of_yojson]
@@ -71,7 +79,7 @@ module Audio_resource = struct
   type t = { data : data } [@@yojson.allow_extra_fields] [@@deriving sexp_of, of_yojson]
 end
 
-let parse_audio_url html =
+let parse_audio_urls html =
   let%bind.Deferred.Or_error group =
     Or_error.try_with (fun () -> Re.exec (force audio_resource_regex) html)
     |> Or_error.tag_s_lazy
@@ -104,19 +112,32 @@ let parse_audio_url html =
               (Comparable.reverse [%compare: int])
               ~f:Audio_resource.bandwidth)
        |> List.hd_exn
-       |> Audio_resource.backup_url
+       |> Audio_resource.urls
      with
-     | [] -> Deferred.Or_error.error_s [%message "No backup URL found for audio resource"]
-     | url :: _ -> Uri.of_string url |> Deferred.Or_error.return)
+     | [] -> Deferred.Or_error.error_s [%message "No URL found for audio resource"]
+     | urls -> List.map urls ~f:Uri.of_string |> Deferred.Or_error.return)
 ;;
 
-let download_audio audio_url =
-  let%bind response, body = Cohttp_async.Client.get ~headers audio_url in
-  let%bind.Deferred.Or_error () = validate_response response in
+let download_audio audio_urls =
+  let%bind.Deferred.Or_error body, audio_url =
+    Deferred.List.fold_until
+      audio_urls
+      ~init:[]
+      ~f:(fun acc audio_url ->
+        let%map response, body = Cohttp_async.Client.get ~headers audio_url in
+        match
+          validate_response response
+          |> Or_error.tag_s_lazy
+               ~tag:[%lazy_sexp { audio_url : string = Uri.to_string audio_url }]
+        with
+        | Ok () -> Continue_or_stop.Stop (Ok (body, audio_url))
+        | Error error -> Continue (error :: acc))
+      ~finish:(fun errors -> Deferred.Or_error.fail (Error.of_list errors))
+  in
   Reader.of_pipe
     (Info.create_s [%message "Audio response body" ~_:(Uri.to_string audio_url : string)])
     (Cohttp_async.Body.to_pipe body)
   |> Deferred.ok
 ;;
 
-let download url = get_html url >>=? parse_audio_url >>=? download_audio
+let download url = get_html url >>=? parse_audio_urls >>=? download_audio
