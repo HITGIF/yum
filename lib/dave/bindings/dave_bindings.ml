83ec83ae5a32b9e47dd2eb877a68befeb42f6d90
hitgif <me@anhai.wang>
2026-01-23 20:58:54 +0800

    .

--- lib/dave/bindings/dave_bindings.ml
+++ lib/dave/bindings/dave_bindings.ml
@@ -1,7 +1,97 @@
+open! Core
 open! Ctypes
 
 module C (F : Cstubs.FOREIGN) = struct
   open! F
+
+  module Make_enum (Arg : sig
+      type t [@@deriving enumerate]
+
+      module Variants : sig
+        val to_rank : t -> int
+      end
+    end) =
+  struct
+    let to_int = Arg.Variants.to_rank
+    let t_to_int = Arg.all |> List.map ~f:(fun t -> to_int t, t) |> Int.Map.of_alist_exn
+    let of_int = Map.find_exn t_to_int
+    let t = view ~read:of_int ~write:to_int int
+  end
+
+  module Codec = struct
+    type t =
+      | Unknown
+      | Opus
+      | Vp8
+      | Vp9
+      | H264
+      | H265
+      | Av1
+    [@@deriving enumerate, variants]
+
+    include functor Make_enum
+  end
+
+  module Media_type = struct
+    type t =
+      | Audio
+      | Video
+    [@@deriving enumerate, variants]
+
+    include functor Make_enum
+  end
+
+  module Encryptor_result_code = struct
+    type t =
+      | Success
+      | Encryption_failure
+      | Missing_key_ratchet
+      | Missing_cryptor
+      | Too_many_attempts
+    [@@deriving enumerate, variants]
+
+    include functor Make_enum
+  end
+
+  module Decryptor_result_code = struct
+    type t =
+      | Success
+      | Decryption_failure
+      | Missing_key_ratchet
+      | Invalid_nonce
+      | Missing_cryptor
+    [@@deriving enumerate, variants]
+
+    include functor Make_enum
+  end
+
+  module Encryptor_stats = struct
+    type t
+
+    let t : t structure typ = structure [%module_name]
+    let passthrough_count = field t [%var_name] uint64_t
+    let encrypt_success_count = field t [%var_name] uint64_t
+    let encrypt_failure_count = field t [%var_name] uint64_t
+    let encrypt_duration = field t [%var_name] uint64_t
+    let encrypt_attempts = field t [%var_name] uint64_t
+    let encrypt_max_attempts = field t [%var_name] uint64_t
+    let encrypt_missing_key_count = field t [%var_name] uint64_t
+    let () = seal t
+  end
+
+  module Decryptor_stats = struct
+    type t
+
+    let t : t structure typ = structure [%module_name]
+    let passthrough_count = field t [%var_name] uint64_t
+    let decrypt_success_count = field t [%var_name] uint64_t
+    let decrypt_failure_count = field t [%var_name] uint64_t
+    let decrypt_duration = field t [%var_name] uint64_t
+    let decrypt_attempts = field t [%var_name] uint64_t
+    let decrypt_missing_key_count = field t [%var_name] uint64_t
+    let decrypt_invalid_nonce_count = field t [%var_name] uint64_t
+    let () = seal t
+  end
 
   let max_supported_protocol_version =
     foreign "daveMaxSupportedProtocolVersion" (void @-> returning int)
@@ -201,5 +291,112 @@ module C (F : Cstubs.FOREIGN) = struct
     let t : t typ = ptr void
     let create = foreign "daveEncryptorCreate" (void @-> returning t)
     let destroy = foreign "daveEncryptorDestroy" (t @-> returning void)
+
+    let set_key_ratchet =
+      foreign "daveEncryptorSetKeyRatchet" (t @-> Key_ratchet.t @-> returning void)
+    ;;
+
+    let set_passthrough_mode =
+      foreign
+        "daveEncryptorSetPassthroughMode"
+        (t @-> bool (* passthroughMode *) @-> returning void)
+    ;;
+
+    let assign_ssrc_to_codec =
+      foreign
+        "daveEncryptorAssignSsrcToCodec"
+        (t @-> uint32_t (* ssrc *) @-> Codec.t @-> returning void)
+    ;;
+
+    let get_protocol_version =
+      foreign "daveEncryptorGetProtocolVersion" (t @-> returning uint16_t)
+    ;;
+
+    let get_max_ciphertext_byte_size =
+      foreign
+        "daveEncryptorGetMaxCiphertextByteSize"
+        (t @-> Media_type.t @-> size_t (* frameSize *) @-> returning size_t)
+    ;;
+
+    let has_key_ratchet = foreign "daveEncryptorHasKeyRatchet" (t @-> returning bool)
+
+    let is_passthrough_mode =
+      foreign "daveEncryptorIsPassthroughMode" (t @-> returning bool)
+    ;;
+
+    let encrypt =
+      foreign
+        "daveEncryptorEncrypt"
+        (t
+         @-> Media_type.t
+         @-> uint32_t (* ssrc *)
+         @-> ptr uint8_t (* frame *)
+         @-> size_t (* frameLength *)
+         @-> ptr uint8_t (* encryptedFrame *)
+         @-> size_t (* encryptedFrameCapacity *)
+         @-> ptr size_t (* bytesWritten *)
+         @-> returning Encryptor_result_code.t)
+    ;;
+
+    let set_protocol_version_changes_callback =
+      foreign
+        "daveEncryptorSetProtocolVersionChangedCallback"
+        (t
+         @-> static_funptr (* callback *)
+               Ctypes.(ptr void (* userData *) @-> returning void)
+         @-> ptr void (* userData *)
+         @-> returning void)
+    ;;
+
+    let get_stats =
+      foreign
+        "daveEncryptorGetStats"
+        (t @-> Media_type.t @-> ptr Encryptor_stats.t @-> returning void)
+    ;;
+  end
+
+  module Decryptor = struct
+    type t = unit ptr
+
+    let t : t typ = ptr void
+    let create = foreign "daveDecryptorCreate" (void @-> returning t)
+    let destroy = foreign "daveDecryptorDestroy" (t @-> returning void)
+
+    let transition_to_key_ratchet =
+      foreign
+        "daveDecryptorTransitionToKeyRatchet"
+        (t @-> Key_ratchet.t @-> returning void)
+    ;;
+
+    let set_passthrough_mode =
+      foreign
+        "daveDecryptorTransitionToPassthroughMode"
+        (t @-> bool (* passthroughMode *) @-> returning void)
+    ;;
+
+    let decrypt =
+      foreign
+        "daveDecryptorDecrypt"
+        (t
+         @-> Media_type.t
+         @-> ptr uint8_t (* encryptedFrame *)
+         @-> size_t (* encryptedFrameCapacity *)
+         @-> ptr uint8_t (* frame *)
+         @-> size_t (* frameCapacity *)
+         @-> ptr size_t (* bytesWritten *)
+         @-> returning Decryptor_result_code.t)
+    ;;
+
+    let get_max_plaintext_byte_size =
+      foreign
+        "daveDecryptorGetMaxPlaintextByteSize"
+        (t @-> Media_type.t @-> size_t (* encryptedFrameSize *) @-> returning size_t)
+    ;;
+
+    let get_stats =
+      foreign
+        "daveDecryptorGetStats"
+        (t @-> Media_type.t @-> ptr Decryptor_stats.t @-> returning void)
+    ;;
   end
 end
