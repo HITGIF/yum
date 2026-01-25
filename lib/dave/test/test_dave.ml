9fb197185803aa8d3901122061a38e6981ac6c4a
hitgif <me@anhai.wang>
2026-01-25 17:18:41 +0800

    .

--- lib/dave/test/test_dave.ml
+++ lib/dave/test/test_dave.ml
@@ -2,9 +2,7 @@ open! Core
 open! Dave
 open! Expect_test_helpers_core
 
-module%test [@name "Dave"] _ = struct
-  let%expect_test "a" =
-    print_s [%sexp (max_supported_protocol_version : int)];
-    [%expect {| 1 |}]
-  ;;
-end
+let%expect_test "max_supported_protocol_version" =
+  print_s [%sexp (max_supported_protocol_version : int)];
+  [%expect {| 1 |}]
+;;
