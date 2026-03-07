1605a28a0b449a9a84864e14ad162f174e9927d6
hitgif <me@anhai.wang>
2026-03-07 11:43:32 +0800

    .

--- lib/Yt_dlp.ml
+++ lib/Yt_dlp.ml
@@ -7,17 +7,9 @@ let default_prog = File_path.Absolute.of_string "/usr/bin/yt-dlp"
 let default_download_args =
   [ "--quiet"; "--no-warnings"; "--no-progress"; "--no-continue" ]
   @ List.concat
-      [ [ "--format"
-        ; "91"
-          (* [bestaudio]s not available due to https://github.com/yt-dlp/yt-dlp/issues/15712#issuecomment-3808702603 *)
-        ]
-      ; [ "--user-agent"
+      [ [ "--user-agent"
         ; "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) \
            Chrome/127.0.6533.103 Mobile Safari/537.36"
-        ]
-      ; [ "--extractor-args"
-        ; "youtube:player_client=default,-android_sdkless"
-          (* https://github.com/yt-dlp/yt-dlp/issues/15712#issuecomment-3808702603 *)
         ]
       ; [ "--output"; "-" ]
       ]
